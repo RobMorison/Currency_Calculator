6494863248a5d5d4b6eb364c20a595a2f9e50f86
Sara Morison <rob.morison32@gmail.com>
2023-05-24 18:13:17 -0400

    Created currency component to hold array of all items + created select map option to select which currency user wants to convert into

---
+++ currency_calculator/src/components/currency.jsx
@@ -0,0 +1,25 @@
+const currency =[ 
+    {
+        name: 'Euro',
+        conversion: 0.92711
+    },
+    {
+        name: 'Pound',
+        conversion: 0.80551
+    },
+    {
+        name: 'Yen',
+        conversion: 138.567
+    },
+    {
+        name: 'Peso',
+        conversion: 17.9312
+    },
+    {
+        name: 'Bitcoin',
+        conversion: 0.00004
+    }
+]
+    
+ 
+export default currency;
--- currency_calculator/src/pages/HomePage.js
+++ currency_calculator/src/pages/HomePage.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
+import currency from './../components/currency'
 
 const HomePage = () => {
 
     const [value, setValue] = useState(0)
+    const [new_value, setNew_Value] = useState([])
 
     console.log(value)
+    console.log(currency)
+    console.log(new_value)
 
     function handleSubmit(event){
         event.preventDefault();
@@ -13,31 +17,24 @@ const HomePage = () => {
 
     return ( 
         <>
-        <h1>Hello World Home Page</h1>
-        <div>
-            You have ${value} USD to convert
-        </div>
         <form>
             <label>Enter value to convert:
                 <input type='integer' value={value} onChange={(event) =>setValue(event.target.value)}></input>
             </label>
             {/* <button onClick={handleSubmit}>Convert</button> */}
+        <div>
+            You have ${value} USD to convert
+        </div>
         </form>
-        <div>
-            Euro = {value * 0.92711}
-        </div>
-        <div>
-            Pound = {value * 0.80551}
-        </div>
-        <div>
-            Yen = {value * 138.567}
-        </div>
-        <div>
-            Peso = {value * 17.9312}
-        </div>
-        <div>
-            Bitcoin = {value * 0.00004}
-        </div>
+        <label> Choose the currency to convert to: 
+            <select onChange={(event) => setNew_Value(event.target.value)}>{currency.map((el, index) => {
+                return(
+                    <option key={index} value={el.conversion}>{el.name}</option>
+                )
+            })}
+            </select>
+        </label>
+        <div> Your ${value} USD would = {value * new_value}</div>
         </>
     
      );
